47f3fe834fb49300f9a0092aded1430bb6ef216e
Lokmanbzd <140338517+Lokmanbzd@users.noreply.github.com>
2023-11-10 17:06:22 -0600

    Update Firyuza.java

--- src/develop/Firyuza.java
+++ src/develop/Firyuza.java
@@ -4,6 +4,6 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Firyuza {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // comment
     }
 }
