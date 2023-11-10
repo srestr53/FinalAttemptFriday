cbc2c5e7697eab3692a789b6a85d711d309bafdd
firyuza <firyuza.badalova@gmail.com>
2023-11-10 18:13:01 -0500

    Firyuza Class created

--- src/develop/Firyuza.java
+++ src/develop/Firyuza.java
@@ -6,6 +6,7 @@ public class Firyuza {
     public static void main(String[] args) {
         System.out.println("Hello World"); // comment iorjrkrtrt
         System.out.println("Wooden Spoon");
+        System.out.println("Hello Cydeo");
 
     }
 }
